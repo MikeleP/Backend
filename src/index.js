10ad3466740afbe2740b8157e8c7d6388c3a2437
MikeleP <mikelepaladin47@gmail.com>
2022-07-27 12:24:42 +0200

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express(); // instanciranje aplikacije
-const port = 3000; // port na kojem će web server slušati
+const port = 3001; // port na kojem će web server slušati
 
 app.use(cors()); // omogući CORS na svim rutama
 app.use(express.json()); //dekodiranje JSON poruke od klijenta
