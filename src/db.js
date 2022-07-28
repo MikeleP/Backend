c6a2ed8c217372a9f5af7c00cddd697effd4052d
MikeleP <mikelepaladin47@gmail.com>
2022-07-28 17:01:34 +0200

    nadopuna "db.js"

---
+++ src/db.js
@@ -0,0 +1,24 @@
+import mongo from "mongodb";
+
+let connection_string =
+  "mongodb+srv://admin:admin@cluster0.fajcph0.mongodb.net/?retryWrites=true&w=majority";
+
+let client = new mongo.MongoClient(connection_string, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db = null;
+
+function isConnected() {
+  return !!client && !!client.topology && client.topology.isConnected();
+}
+
+export default async () => {
+  if (!db || !isConnected()) {
+    await client.connect();
+    db = client.db("govedo");
+    console.log("Connected OK");
+  }
+  return db;
+};
