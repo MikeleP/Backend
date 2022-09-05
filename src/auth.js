8a2bb1bab4f53fec4ae6ae6169ef9343d165e32e
MikeleP <mikelepaladin47@gmail.com>
2022-09-05 14:09:54 +0200

    bcrypt

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,17 @@
 import mongo from "mongodb";
 import connect from "./db.js";
+import bcrypt from "bcrypt";
 
 export default {
   async registerUser(userData) {
     let db = await connect();
 
-    await db.collection("users").insertOne(userData);
+    let doc = {
+      username: userData.username,
+      password: await bcrypt.hash(userData.password, 8),
+      farm_code: userData.farm_code,
+    };
+
+    await db.collection("users").insertOne(doc);
   },
 };
